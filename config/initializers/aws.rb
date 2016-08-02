9173a56ffbb378a2fad077feb4a01b443360f82f
Austin Putman <austin@rawfingertips.com>
2016-08-02 13:37:40 -0400

    manually add ses delivery method

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,13 @@
 if ENV['S3_BUCKET_NAME']
-  Aws.config.update(
-    {
-      region: ENV['S3_BUCKET_REGION'],
-      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-    })
+  Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']).tap do |aws_credentials|
+    Aws.config.update(
+      {
+        region: ENV['S3_BUCKET_REGION'],
+        credentials: aws_credentials
+      })
 
-  S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET_NAME'])
+    S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET_NAME'])
 
-  # Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['S3_BUCKET_REGION'])
+    Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['S3_BUCKET_REGION'])
+  end
 end
