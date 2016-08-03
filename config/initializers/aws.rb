226f5d33cb595dd80932eecc4f0b9f46023da602
Austin Putman <austin@rawfingertips.com>
2016-08-03 11:27:57 -0400

    standardize region for S3 and SES

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,12 +2,12 @@ if ENV['S3_BUCKET_NAME']
   Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']).tap do |aws_credentials|
     Aws.config.update(
       {
-        region: ENV['S3_BUCKET_REGION'],
+        region: ENV['AWS_REGION'],
         credentials: aws_credentials
       })
 
     S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET_NAME'])
 
-    Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['S3_BUCKET_REGION'])
+    Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: ENV['AWS_REGION'])
   end
 end
