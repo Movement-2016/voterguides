6a84293722bb9467dcae329248eec8221cc04896
Austin Putman <austin@rawfingertips.com>
2016-08-03 08:36:15 -0400

    endorsements close automatically when re-ordering; also toggle button for manual mgmt

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -7,6 +7,13 @@ function sortable_endorsements(selector) {
     handle: '.endorsement_handle',
     draggable: '.endorsement_list_item',
     ghostClass: 'endorsement_list_placeholder',
+    // dragging started
+    onStart: function (/**Event*/evt) {
+      $('.endorsement_list_item .panel-collapse.in').addClass('js-dragging-in').removeClass('in');
+    },
+    onEnd: function(evt) {
+      $('.endorsement_list_item .panel-collapse.js-dragging-in').addClass('in').removeClass('js-dragging-in');
+    },
     onUpdate: function (evt) {
 
       var itemEl = evt.item;  // dragged HTMLElement
@@ -27,7 +34,23 @@ function display_endorsements(selector) {
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
   var endorsements = $(".panel-collapse", selector);
   if (w > 768 && endorsements.length > 0) {
-    endorsements.addClass('in');
+    toggle_all_endorsements(new jQuery.Event);
   }
+  $('#toggle_all_endorsements').on('click', toggle_all_endorsements);
   $('input[type=checkbox][data-toggle^=vg_toggle]').bootstrapToggle();
+}
+
+function toggle_all_endorsements(ev) {
+  ev.preventDefault();
+  var selector = "#endorsement_list";
+  var panel_toggle = $('#toggle_all_endorsements');
+  if(panel_toggle.attr('data-state') == 'closed') {
+    $('.panel-collapse', selector).addClass('in');
+    panel_toggle.text(panel_toggle.data('open'));
+    panel_toggle.attr('data-state', 'open');
+  } else {
+    $('.panel-collapse', selector).removeClass('in');
+    panel_toggle.text(panel_toggle.data('closed'));
+    panel_toggle.attr('data-state', 'closed');
+  }
 }
